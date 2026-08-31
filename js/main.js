77eb8c9f84649cb79214c347047a9145cece0335
Malkas Guru Dev <dev@malkasguru.lv>
2026-08-31 15:19:09 +0300

    Change Berkubos un maisos to Berkubos vai maisos, bump cache to v2.6.2/v23/v13

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function() {
-    const jsVersion = '7';
+    const jsVersion = '8';
     const scripts = document.querySelectorAll('script[src*="main.js"]');
     scripts.forEach(script => {
         const src = script.getAttribute('src').split('?')[0];
